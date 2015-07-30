37ee4e1bd5b2e9260740ab560991e324c8738f39
dharmik0810 <dharmik0810@gmail.com>
2015-07-30 11:45:13 -0500

    Create store.js

---
+++ js/store.js
@@ -0,0 +1,13 @@
+function store() {
+    this.products = [
+        new product("men1", "Men's Round Neck", "Lee Printed Men's Round Neck T-Shirt!", 12.99),
+        new product("men2", "Men's Polo T-Shirt", "United Colors of Benetton Striped Men's Polo T-Shirt", 16.99),
+        new product("men3", "Men's Polo Neck T-Shirt", "Campus Sutra Solid Men's Polo Neck T-Shirt!!", 9.99),
+        new product("women1", "Women's Shift Dress", "fabric & care Machine wash.Imported V-neckline.Short dolman sleeves.", 18.99),
+        new product("women2", "Women's babydoll Dress", "Glam Quotient Solid Babydoll fit & sizing Relaxed fit.Dress hits above knee!", 29.99),
+        new product("women3", "Women's Saree", "Sargam Fashion Embriodered Bollywood Georgette Sari!!", 49.99),
+        new product("kids1", "Kids Printed Choli", "Dotnditto Printed Girl's Ghagra, Choli, Dupatta Set",22.99),
+        new product("kids2", "Kids Round Neck T-shirt", "Yellow Dots Striped Boy's Round Neck T-Shirt", 18.99),
+        new product("kids3", "Kids waist dress", "Aarika Baby Girl's Empire Waist Dress", 14.99),
+    ];
+}
